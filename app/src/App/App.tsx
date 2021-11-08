d01447adc3c2deae7448614d27d0a18765ba7d53
Cedric Coret <ccoret@blizzard.com>
2021-11-08 15:11:44 +0100

    bouton

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -3,9 +3,17 @@ import './App.css';
 import Button from './components/Button/Button';
 
 function App() {
+  
+  let counter =0;
+
   return (
     <div className="App">
-      <Button>blabla</Button>
+      <h1>Compteur: {counter}</h1>
+
+
+      <Button onButtonClick={(arg) : void => { counter++; }}>Ajout +1</Button>
+      <Button bgColor='#ff0' onButtonClick={() => {}}>Clickez</Button>
+      <Button onButtonClick={() => {}} color='#444' style={{textDecoration:'underline', fontWeight:'bold'}}>-1</Button>
     </div>
   );
 }
--- app/src/App/components/Button/Button.js
+++ app/src/App/components/Button/Button.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './Button.css';
+import React, {useState} from 'react';
+import style from './Button.module.css';
+import PropTypes from 'prop-types';
 
 const Button=(props) => {
 
+    const[isClicked, setisClicked] = useState({clicked:false, uneValue:0});
     console.log(props);
 
-    return (<div className="Button">{props.children}</div>);
+    return (<div onClick={(evt) => {
+        setisClicked({...isClicked, clicked:true});
+        setTimeout(()=>{
+            setisClicked({...isClicked, clicked:false});
+        },300);
+
+        props.onButtonClick();
+        console.log(evt);
+      }} className={`${style.Button}${isClicked.clicked?' '+style.clicked:''}`}
+      style={{backgroundColor:props.bgColor, color:props.color, ...props.style}}
+      >{props.children}</div>);
 }
 
+Button.propTypes={
+    children: PropTypes.any.isRequired,
+    onButtonClick: PropTypes.func.isRequired,
+    bgColor: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    style: PropTypes.object
+};
+Button.defaultProps={
+    children: "Aucun texte",
+    bgColor: '#00f',
+    color: '#0ff'
+};
 export default Button;
